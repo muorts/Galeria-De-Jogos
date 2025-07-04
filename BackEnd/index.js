bdf256ea21ff691ce1984ed51a30bf7d529f7df2
muorts <muortegapereira.07@gmail.com>
2025-07-03 22:58:34 -0300

    Trabalhando no JVSC do index

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,4 +1,4 @@
-document.addEventListener('PopupPesquisa', () => {
+document.addEventListener('DOMContentLoaded', () => {
     const botaoPesquisar = document.getElementById('pesquisa');
     const Pesquisa = document.getElementById('pop-pesquisa');
     const inputPesquisa = document.getElementById('input-pesquisa');
@@ -9,6 +9,43 @@ document.addEventListener('PopupPesquisa', () => {
         inputPesquisa.focus();
     }
 
+    function FecharPop() {
+        Pesquisa.classList.remove('active');
+        inputPesquisa.value = ''; 
+    }
 
-}) 
+    botaoPesquisar.addEventListener('click', (event) => {
+        event.stopPropagation();
+        if (Pesquisa.classList.contains('active'))
+        {
+            FecharPop();
+        }
+        else 
+        {
+            AbrirPop();
+        }
+    });
+
+    submeterPesquisa.addEventListener('click', () => {
+        const query = inputPesquisa.value.trim();
+        if (query)
+        {
+            alert('Voce pesquisou por: ' + query);
+        }
+        else 
+        {
+            alert('Por favor, digite algo...');
+        }
+    });
+
+    document.addEventListener('click', (event) => {
+        if (!Pesquisar.contains(event.target) && !botaoPesquisar.contains(event.target))
+        {
+            closePesquisar();
+        }
+
+    });
+
+
+});
 
