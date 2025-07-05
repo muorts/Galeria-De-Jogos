38e7201b07f4fe8008a39b4090e6e9aed336b043
muorts <muortegapereira.07@gmail.com>
2025-07-04 23:03:28 -0300

    botao pesquisa feito, falta css

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,22 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const botaoPesquisar = document.getElementById('pesquisa');
-    const Pesquisa = document.getElementById('pop-pesquisa');
+    const PopPesquisa = document.getElementById('pop-pesquisa');
     const inputPesquisa = document.getElementById('input-pesquisa');
-    const submeterPesquisa = document.getElementById('botao-pesquisa');
 
     function AbrirPop() {
-        submeterPesquisa.classList.add('active');
+        PopPesquisa.classList.add('active');
         inputPesquisa.focus();
     }
 
     function FecharPop() {
-        Pesquisa.classList.remove('active');
+        PopPesquisa.classList.remove('active');
         inputPesquisa.value = ''; 
     }
 
     botaoPesquisar.addEventListener('click', (event) => {
         event.stopPropagation();
-        if (Pesquisa.classList.contains('active'))
+        if (PopPesquisa.classList.contains('active'))
         {
             FecharPop();
         }
@@ -26,22 +25,28 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    submeterPesquisa.addEventListener('click', () => {
-        const query = inputPesquisa.value.trim();
-        if (query)
+    inputPesquisa.addEventListener('keydown', (event) => {
+        if (event.key == 'Enter')
         {
-            alert('Voce pesquisou por: ' + query);
-        }
-        else 
-        {
-            alert('Por favor, digite algo...');
+            event.preventDefault();
+            const query = inputPesquisa.value.trim();
+            if (query)
+            {
+                alert('Voce pesquisou por ' + query);
+                FecharPop();
+            }
+
+            else 
+            {
+                alert('Por favor, digite algo...');
+            }
         }
     });
 
     document.addEventListener('click', (event) => {
-        if (!Pesquisar.contains(event.target) && !botaoPesquisar.contains(event.target))
+        if (!PopPesquisa.contains(event.target) && !botaoPesquisar.contains(event.target))
         {
-            closePesquisar();
+            FecharPop();
         }
 
     });
